bd7653546635ba23aa7ca1a2039b13d67df15deb
Aleksi Ojansivu <aleksi.ojansivu@gmail.com>
2025-02-03 17:45:44 +0200

    added react router and sign in page, ex 10.6

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,6 +1,7 @@
 import { View, StyleSheet, Text, Pressable } from 'react-native';
 import Constants from 'expo-constants';
 import theme from '../theme';
+import { Link } from 'react-router-native';
 
 const styles = StyleSheet.create({
   container: {
@@ -8,6 +9,8 @@ const styles = StyleSheet.create({
     backgroundColor: theme.colors.appBarBackground,
     paddingHorizontal: 10,
     paddingBottom: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
   },
   
 });
@@ -16,9 +19,18 @@ const AppBar = () => {
   return (
   <View style={styles.container}>
     <Pressable>
-        <Text style={{color: 'white', fontSize: theme.fontSizes.subheading}}>
+      <Link to={'/'}>
+      <Text style={{color: 'white', fontSize: theme.fontSizes.subheading}}>
             Repositories
         </Text>
+      </Link>
+    </Pressable>
+    <Pressable>
+      <Link to={'/signin'}>
+      <Text style={{color: 'white', fontSize: theme.fontSizes.subheading}}>
+            Sign in
+        </Text>
+      </Link>
     </Pressable>
   </View>);
 };
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,9 @@
 import Constants from 'expo-constants';
 import { Text, StyleSheet, View } from 'react-native';
+import { Route, Routes, Navigate } from 'react-router-native';
 import RepositoryList from './RepositoryList';
 import AppBar from './AppBar';
+import SignIn from './SignIn';
 
 const styles = StyleSheet.create({
   container: {
@@ -16,7 +18,11 @@ const Main = () => {
   return (
     <View style={styles.container}>
       <AppBar />
-      <RepositoryList />
+      <Routes>
+        <Route path='/' element={<RepositoryList />} />
+        <Route path='/signin' element={<SignIn />} />
+        <Route path='*' element={<Navigate to={"/"} replace />} />
+      </Routes>
     </View>
   );
 };
