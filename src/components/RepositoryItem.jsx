d8b0f3059ec8cf04c86e1c76677208451344279f
Aleksi Ojansivu <aleksi.ojansivu@gmail.com>
2025-02-03 15:35:09 +0200

    modified list to be more readable, ex 10.5

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,16 +1,62 @@
-import { View, Text, Image } from "react-native"
+import { View, Text, Image, StyleSheet } from "react-native"
 import theme from "../theme"
+
+
+const styles = StyleSheet.create({
+  column: {
+    flexDirection: 'column',
+    padding: 10,
+    flexShrink: 1
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-around'
+  },
+  languageTag: {
+    backgroundColor: theme.colors.primary,
+    color: 'white',
+    padding: 5,
+    borderRadius: 5
+  }
+});
+
+const formatCount = (count) => count > 1000 ? `${(count / 1000).toFixed(1)}k` : count.toString()
 
 const RepositoryItem = ({repository}) => (
     <View style={{backgroundColor: 'white'}}>
-        <Image style={theme.logo} source={{uri: repository.ownerAvatarUrl}}></Image>
-        <Text>Full name: {repository.fullName}</Text>
-        <Text>Description: {repository.description}</Text>
-        <Text>Language: {repository.language}</Text>
-        <Text>Stars: {repository.stargazersCount}</Text>
-        <Text>Forks: {repository.forksCount}</Text>
-        <Text>Reviews: {repository.reviewCount}</Text>
-        <Text>Rating: {repository.ratingAverage}</Text>
+        <View style={{flexDirection: 'row'}}>
+            <View style={{padding: 10}}>
+                <Image style={theme.logo} source={{uri: repository.ownerAvatarUrl}}></Image>
+            </View>
+            
+            <View style={styles.column}>
+                <Text style={{fontWeight: 'bold'}}>{repository.fullName}</Text>
+                <Text>Description: {repository.description}</Text>
+                <View style={{alignSelf: 'flex-start'}}>
+                    <Text style={styles.languageTag}>{repository.language}</Text>
+                </View>
+            </View>
+        </View>
+        
+        
+        <View style={styles.row}>
+            <View style={{flexDirection: 'column'}}>
+                <Text style={{fontWeight: 'bold'}}>{formatCount(repository.stargazersCount)}</Text>
+                <Text>Stars</Text>
+            </View>
+            <View style={{flexDirection: 'column'}}>
+                <Text style={{fontWeight: 'bold'}}>{formatCount(repository.forksCount)}</Text>
+                <Text>Forks</Text>
+            </View>
+            <View style={{flexDirection: 'column'}}>
+                <Text style={{fontWeight: 'bold'}}>{formatCount(repository.reviewCount)}</Text>
+                <Text>Reviews</Text>
+            </View>
+            <View style={{flexDirection: 'column'}}>
+                <Text style={{fontWeight: 'bold'}}>{formatCount(repository.ratingAverage)}</Text>
+                <Text>Rating</Text>
+            </View>
+        </View>
     </View>
 )
 
