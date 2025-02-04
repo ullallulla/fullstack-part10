cdfa8ad16007b3fbc2aad6785d966f40b5c4987e
Aleksi Ojansivu <aleksi.ojansivu@gmail.com>
2025-02-04 11:43:04 +0200

    simple sign in form with formik, ex 10.8

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,7 +1,46 @@
-import {Text} from 'react-native';
+import {Text, TextInput, View, Pressable} from 'react-native';
+import { useFormik } from 'formik';
 
+const initialValues = {
+    username: '',
+    password: ''
+}
+
+
+const onSubmit = (values) => {
+    console.log(values);
+  };
+
+const SignInForm = ( { onSubmit } ) => {
+    const formik = useFormik({
+        initialValues,
+        onSubmit
+    })
+
+    return (
+        <View>
+            <TextInput 
+            placeholder='username'
+            value={formik.values.username}
+            onChangeText={formik.handleChange('username')}
+            />
+            <TextInput 
+            placeholder='password'
+            value={formik.values.password}
+            onChangeText={formik.handleChange('password')}
+            secureTextEntry
+            />
+            <Pressable onPress={formik.handleSubmit}>
+                <Text>Sign in</Text>
+            </Pressable>
+        </View>
+    )
+}
 const SignIn = () => {
-  return <Text>The sign-in view</Text>;
+
+    return (
+        <SignInForm onSubmit={onSubmit}/>
+    )
 };
 
 export default SignIn;
