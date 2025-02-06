1ca842102fc69967d5d58c1a496143a4ecbd9bb7
Aleksi Ojansivu <aleksi.ojansivu@gmail.com>
2025-02-06 14:23:42 +0200

    added support for environment variables and moved apollo uri to env variable, ex 10.12

--- App.js
+++ App.js
@@ -2,10 +2,13 @@ import { NativeRouter } from 'react-router-native';
 import { ApolloProvider } from '@apollo/client';
 import Main from './src/components/Main';
 import createApolloClient from './src/utils/apolloClient';
+import Constants from 'expo-constants';
 
 const apolloClient = createApolloClient();
 
 const App = () => {
+  console.log(Constants.expoConfig, 'EXPO')
+
   return (
   <>
     <NativeRouter>
--- src/utils/apolloClient.js
+++ src/utils/apolloClient.js
@@ -1,9 +1,10 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
+import Constants from 'expo-constants'
 
 
 const createApolloClient = () => {
   return new ApolloClient({
-    uri: 'http://172.28.210.239:4000/graphql',
+    uri: `http://${Constants.expoConfig.extra.apolloURI}:4000/graphql`,
     cache: new InMemoryCache(),
   });
 };
