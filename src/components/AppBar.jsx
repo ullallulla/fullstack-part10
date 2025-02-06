d71b512e0dd7ed0dc992df5a9df46f1ac7248695
Aleksi Ojansivu <aleksi.ojansivu@gmail.com>
2025-02-07 01:34:23 +0200

    added a query for getting user, changed appbar to show sign out button when user is logged in, ex 10.16

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -3,6 +3,10 @@ import Constants from 'expo-constants';
 import theme from '../theme';
 import { Link } from 'react-router-native';
 import Text from './Text';
+import { useQuery } from '@apollo/client';
+import useAuthStorage from '../hooks/useAuthStorage';
+import { useApolloClient } from '@apollo/client';
+import { ME } from '../graphql/queries';
 
 const styles = StyleSheet.create({
   container: {
@@ -17,25 +21,65 @@ const styles = StyleSheet.create({
 });
 
 const AppBar = () => {
+  const { data, loading } = useQuery(ME)
+
+  const authStorage = useAuthStorage()
+  const apolloClient = useApolloClient()
+
+  const logout = async () =>{
+    await authStorage.removeAccessToken()
+    apolloClient.resetStore()
+  
+  }
+
+  if (loading) {
+      return (
+        <View>
+          <Text>Loading...</Text>
+        </View>
+      )
+  }
+
+  if (data.me) {
+    return (
+      <View style={styles.container}>
+        <ScrollView horizontal contentContainerStyle={{gap: 20}}>
+          <Pressable>
+            <Link to={'/'}>
+            <Text style={{color: 'white'}} fontSize="subheading">
+                  Repositories
+              </Text>
+            </Link>
+          </Pressable>
+          <Pressable onPress={logout}>
+            <Text style={{color: 'white'}} fontSize="subheading">
+                  Sign out
+            </Text>
+        </Pressable>
+      </ScrollView>
+    </View>
+    )
+  }
+
   return (
-  <View style={styles.container}>
-    <ScrollView horizontal contentContainerStyle={{gap: 20}}>
-      <Pressable>
-        <Link to={'/'}>
-        <Text style={{color: 'white'}} fontSize="subheading">
-              Repositories
-          </Text>
-        </Link>
-      </Pressable>
-      <Pressable>
-        <Link to={'/signin'}>
-        <Text style={{color: 'white'}} fontSize="subheading">
-              Sign in
-          </Text>
-        </Link>
-      </Pressable>
-    </ScrollView>
-  </View>);
+    <View style={styles.container}>
+      <ScrollView horizontal contentContainerStyle={{gap: 20}}>
+        <Pressable>
+          <Link to={'/'}>
+          <Text style={{color: 'white'}} fontSize="subheading">
+                Repositories
+            </Text>
+          </Link>
+        </Pressable>
+        <Pressable>
+          <Link to={'/signin'}>
+          <Text style={{color: 'white'}} fontSize="subheading">
+                Sign in
+            </Text>
+          </Link>
+        </Pressable>
+      </ScrollView>
+    </View>); 
 };
 
 export default AppBar;
